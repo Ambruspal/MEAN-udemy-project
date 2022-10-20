aac0cb2d37d5f9f7d4a26a59422e6f2158425bc6
Ambruspal <phaal83@gmail.com>
2022-10-20 10:52:47 +0200

    Alkalmazás inicializálva

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MEAN
+# MEAN-Angular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.1.4.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MEAN'`, () => {
+  it(`should have as title 'MEAN-Angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MEAN');
+    expect(app.title).toEqual('MEAN-Angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('MEAN app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'MEAN-Angular app is running!'
+    );
   });
 });
